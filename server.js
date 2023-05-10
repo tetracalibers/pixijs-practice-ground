9b97b2f36ed8b4eaacdf076ab6d8dd0f10e92820
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-10 14:13:53 +0900

    globのバージョンをv8に

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const serveStatic = require("serve-static")
 const glob = require("glob")
 
 connect()
-  .use(serveStatic(__dirname + "/"))
+  .use(serveStatic(__dirname))
   .listen(8080, () => {
-    glob("**/*.html", (_, files) => {
-      files.forEach((file) => {
+    glob("./**/*.html", (_, files) => {
+      files?.forEach((file) => {
         const url = new URL(file, "http://localhost:8080")
         console.log(url.href)
       })
